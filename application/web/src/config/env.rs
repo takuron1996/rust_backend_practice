2333540ae680a45652edee40c7f6deb3b392d3bd
taku ikegami <takuron1996@gmail.com>
2024-06-30 13:41:30 +0900

    CORSの設定を環境変数で行えるように実装

---
+++ application/web/src/config/env.rs
@@ -0,0 +1,29 @@
+use common::config::env::deserialize_with_env;
+use confy::{self, ConfyError};
+use log::error;
+use serde::{Deserialize, Serialize};
+use std::process;
+/// CORS関連
+#[derive(Default, Debug, Serialize, Deserialize)]
+pub struct CorsConfig {
+    #[serde(deserialize_with = "deserialize_with_env")]
+    pub allowed_origin: String,
+}
+
+#[derive(Default, Debug, Serialize, Deserialize)]
+pub struct Config {
+    /// CORSで許可するオリジン
+    pub cors: CorsConfig,
+}
+
+/// YAMLからコンフィグの値を取得
+pub fn get_config() -> Config {
+    let cfg: Result<Config, ConfyError> = confy::load_path("web/src/config/config.yml");
+    match cfg {
+        Ok(cfg) => cfg,
+        Err(e) => {
+            error!("{}", e);
+            process::exit(1);
+        }
+    }
+}
